69aaddb20d7d6c8b2243708cc02a7886d1c39bda
Peter <peter.prentiss@gmail.com>
2018-04-01 21:48:55 -0500

    Initial setup

---
+++ ControllersAndActions/Controllers/PocoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace ControllersAndActions.Controllers
+{
+    public class PocoController : Controller
+    {
+        public ViewResult Index() =>
+            View("Result", $"This is a derived controller");
+    }
+}
